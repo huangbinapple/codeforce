88a8a368ab9abe80e4fb8328ef5d10ff1d6c20bd
Bin Huang <huangbin@hbdemacmini.lan>
2019-03-21 10:09:50 +0800

    Probably identified the error is due to too many recursions.

--- 547_g.py
+++ 547_g.py
@@ -1,57 +1,48 @@
-# import time
+import time
+import sys
+
+sys.setrecursionlimit(2000000)
 
 def set_color(edge, colors, color):
-    if edge in colors:
-        colors[edge] = color
-    else:
-        colors[(edge[1], edge[0])] = color
+    colors[edge] = color
+    colors[(edge[1], edge[0])] = color
 
 def set_node_color(node, parent, parent_color, index, bad_nodes, colors):
-    is_bad_node = node in bad_nodes
     color = 1
     for child_node in (child for child in index[node] if child != parent):
         if color == parent_color:
             color += 1
-        new_color = parent_color if is_bad_node else color
+        new_color = parent_color if node in bad_nodes else color
         color += 1
         set_color((node, child_node), colors, new_color)
         set_node_color(child_node, node, new_color, index, bad_nodes, colors)
 
 def solve(n, k, edges):
-    # print('k:', k)
     colors = {edge: None for edge in edges}
     index = {i : [] for i in range(1, n + 1)}
     for a, b in edges:
         index[a].append(b)
         index[b].append(a)
-    # for kk, v in index.items():
-        # print(kk, v)
-    nodes = list(range(1, n + 1))
-    nodes.sort(key=lambda x: len(index[x]), reverse=True)
-    # print(nodes)
+    nodes = sorted(list(range(1, n + 1)), key=lambda x: len(index[x]), reverse=True)
     bad_nodes = set(nodes[:k])
-    # print('bad nodes:', bad_nodes)
     set_node_color(nodes[k], None, None, index, bad_nodes, colors)
-    result = []
-    for a, b in edges:
-        if (a, b) in colors:
-            result.append(colors[(a, b)])
-        else:
-            result.append(colors[(b, a)])
-    return result
+    return [colors[edge] for edge in edges]
 
 def main():
-    n, k = map(int, input().split())
-    edges = []
-    for i in range(n - 1):
-        edges.append(tuple(map(int, input().split())))
-    # tick = time.time()
+    # n, k = map(int, input().split())
+    # edges = []
+    # for i in range(n - 1):
+        # edges.append(tuple(map(int, input().split())))
+    n, k = 200000, 0
+    edges = [(n, n + 1) for n in range(1, n)]
+    # n, k = 6, 2
+    # edges = [(1, 4), (4, 3), (3, 5), (3, 6), (5, 2)]
+    tick = time.time()
     result = solve(n, k, edges)
-    # print('result:', result)
     print(len(set(result)))
     print(' '.join(map(str, result)))
-    # tock = time.time()
-    # print('T:', round(tock - tick, 5))
+    tock = time.time()
+    print('T:', round(tock - tick, 5))
 
 if __name__ == "__main__":
     main()
