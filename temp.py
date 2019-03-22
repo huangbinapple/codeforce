af477a416fd752e32c41782db7354c71bad543bb
Bin Huang <huangbin@hbdemacmini.local>
2019-03-22 21:11:39 +0800

    Add a template file.

---
+++ temp.py
@@ -0,0 +1,18 @@
+# import time
+
+
+def solve():
+    pass
+    
+def main():
+    # Deal input here.
+
+    
+    # tick = time.time()
+    print(solve())
+    # tock = time.time()
+    # print(round(tock - tick, 5))
+
+
+if __name__ == '__main__':
+    main()
