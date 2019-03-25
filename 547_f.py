af0cddff2a26f65c667cf0a19c61c4b6dcf9ac60
Huang Bin <huangbinapple@gmail.com>
2019-03-25 14:19:10 +0800

    Submit: PASS! RECORD! change the schedule algorithm to make it better.

--- 547_f.py
+++ 547_f.py
@@ -1,57 +1,21 @@
 import time
 
 
-def index(key, item, index):
-    if key in index:
-        index[key].append(item)
-    else:
-        index[key] = [item]
-
-def is_in_order(l):
-    if not l:
-        return True
-    for i in range(1, len(l)):
-        old, new = l[i - 1], l[i]
-        if new[0] < old[0]:
-            return False
-        elif new[0] == old[0]:
-            if new[1] < old[1]:
-                return False
-    return True
-
 def schedule2(times):
-    # print(times)
-    assert is_in_order(times)
     result = []
     a_min = 0
     for s, e in times:
         if s >= a_min:
             result.append((s, e))
             a_min = e
-    print(len(result), ':', times)
     return result
 
-def schedule(times):
-    index_by_b = {}
-    for time in times:
-        index(time[1], time, index_by_b)
-    b_keys = sorted(list(index_by_b.keys()))
-    result = []
-    a_min = 0
-    # Get interval with minimun end time whose start time >= a_min.
-    for end_time in b_keys:
-        start = index_by_b[end_time].pop()[0]
-        if start >= a_min:
-            result.append((start, end_time)) 
-            a_min = end_time
-    return result
-                
 def solve(n, a_l):
     index_by_sum = {}
-    for i in range(n):
+    for j in range(1, n + 1):
         sum_ = 0
-        for j in range(i + 1, n + 1):
-            sum_ += a_l[j - 1]
+        for i in range(j - 1, -1, -1):
+            sum_ += a_l[i]
             if sum_ in index_by_sum:
                 index_by_sum[sum_].append((i, j))
             else:
@@ -64,10 +28,8 @@ def solve(n, a_l):
     return result
 
 def test():
-    n = 7
-    a_l = [4, 1, 2, 2, 1, 5, 3]
-    # n = 1500
-    # a_l = list(range(1, n + 1))
+    n = 1500
+    a_l = list(range(1, n + 1))
     tick = time.time()
     result = solve(n, a_l)
     tock = time.time()
