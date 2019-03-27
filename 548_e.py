c7e58b5384865e5ce4ac5ea5f21f3f9f4f33bf3a
Huang Bin <huangbinapple@gmail.com>
2019-03-27 22:17:58 +0800

    Submit: Time limit exceeded on test 13(pypy3). Big refactoring, but slower...

--- 548_e.py
+++ 548_e.py
@@ -1,79 +1,78 @@
-# import time
+import time
 import itertools
 
-def delete_node(children, node):
-    for v in children:
-        v.discard(node)
-
-def find_longest_path(children, clubs, frontier):
-    clubs_visited = []
-    path = []
-    top_path = []
-    result = [0, 0]
+def dfs(p_to_c, start, used):
+    size = len(p_to_c)
+    used_copy = used.copy()
+    # print('start:', start)
+    max_, depth = 0, 0
+    frontier = [ele for ele in p_to_c[start] if not used[ele]]
+    # print('f:', frontier)
     while frontier:
         ele = frontier.pop()
-        if ele is None:
-            clubs_visited.pop()
-            path.pop()
+        if type(ele) == tuple:
+            used[ele[0]] = False
+            depth -= 1
         else:
-            frontier.append(None)
-            new_nodes = list(filter(lambda x: clubs[x] not in clubs_visited, children[ele]))
+            frontier.append((ele,))
+            used[ele] = True
+            depth += 1
+            if start + depth < size:
+                new_nodes = [e for e in p_to_c[start + depth] if not used[e]]
+            else:
+                new_nodes = []
+            # print('new_nodes:', new_nodes)
             frontier.extend(new_nodes)
-            clubs_visited.append(clubs[ele])
-            path.append(ele)
             if not new_nodes:
                 # print('ya')
-                if len(clubs_visited) > result[0]:
-                    top_path = path.copy()
-                result.append(len(clubs_visited))
-                result.sort(reverse=True)
-                result.pop()
-    return result, top_path
+                max_ = max(max_, depth)
+                used_copy = used.copy()
+    return (max_, used_copy)
 
 def solve(potentials, clubs, dies, m, n, d):
-    potential_index = {}
-    for i, p in enumerate(potentials):
-        if p in potential_index:
-            potential_index[p].add(i)
+    p_to_c = [set() for i in range(5000)]
+    dies_ = set(dies)
+    used = [False for i in range(m)]
+    for i in filter(lambda x: x not in dies_, range(n)):
+        potential = potentials[i]
+        club = clubs[i]
+        p_to_c[potential].add(club)
+    mex, used = dfs(p_to_c, 0, used)
+    result = [mex]
+    # print(p_to_c)
+    # print(used)
+    while len(dies) > 1:
+        # print()
+        # Add new node.
+        new = dies.pop()
+        new_potential = potentials[new]
+        new_club = clubs[new]
+        # print('new:', new, new_potential, new_club)
+        p_to_c[new_potential].add(new_club)
+        # print(p_to_c)
+        # print(used)
+        # print('mex:', mex)
+        if new_potential != mex:
+            used = [False for i in range(m)]
+            mex, used = dfs(p_to_c, 0, used)
+        elif used[new_club]:
+            # print('haha')
+            used = [False for i in range(m)]
+            mex, used = dfs(p_to_c, 0, used)
         else:
-            potential_index[p] = set([i])
-    # print('p_index:')
-    # print(potential_index)
-    # print('club_index')
-    # print(club_index)
-    childrens = [set() for i in range(n)]
-    potentials_index_key = sorted(list(potential_index.keys()))
-    for i in range(1, len(potentials_index_key)):
-        last_po, current_po = potentials_index_key[i - 1], potentials_index_key[i]
-        if last_po == current_po - 1:
-            for a, b in filter(lambda x: clubs[x[0]] != clubs[x[1]],
-                    itertools.product(potential_index[last_po], potential_index[current_po])):
-                childrens[a].add(b)
-    result = []
-    top_two_length, top_path = [0, 0], []
-    potentials_0 = potential_index[0]
-    for i in range(d):
-        delete_node(childrens, dies[i])
-        potentials_0.discard(dies[i])
-        try:
-            die_index = top_path.index(dies[i])
-        except ValueError:
-            die_index = -1
-        if top_path and die_index == -1:
-            pass
-        elif top_path and die_index >= top_two_length[1]:
-            top_two_length[0] = die_index
-            top_path = top_path[:die_index]
-        else:
-            top_two_length, top_path = find_longest_path(childrens, clubs, list(potentials_0))
-        result.append(top_two_length[0])
-    return result
+            # print('yoyo')
+            mex_delta, used = dfs(p_to_c, mex, used)
+            mex += mex_delta
+        # print('mex:', mex)
+        result.append(mex)
+    return reversed(result)
+
 
 def main():
     # Deal with input.
     n , m = map(int, input().split())
     potentials = list(map(int, input().split()))
-    clubs = list(map(int, input().split()))
+    clubs = list(map(lambda x: int(x) - 1, input().split()))
     d = int(input())
     dies = []
     for _ in range(d):
@@ -84,24 +83,23 @@ def main():
     # Deal with output.
     for ele in result:
         print(ele)
-
     # Printe time.
     # print('T:', round(tock - tick ,5))
 
 def test():
-    n, m = 1000, 1000
+    n, m = 5000, 5000
     potentials = list(range(n))
     clubs = list(range(m))
     dies = list(reversed(range(1, n + 1)))
     d = n
-    # tick = time.time()
+    tick = time.time()
     result = solve(potentials, clubs, dies, m, n, d)
-    # tock = time.time()
+    tock = time.time()
     # Deal with output.
     for ele in result:
         print(ele)
     # Printe time.
-    # print('T:', round(tock - tick ,5))
+    print('T:', round(tock - tick ,5))
 
 
 if __name__ == "__main__":
